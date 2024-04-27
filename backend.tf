c55dd1ab66f6c9374c05e919a7879b68f4a402ba
IamBrinda <167959820+IamBrinda@users.noreply.github.com>
2024-04-27 22:58:52 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "brinda-statefile"
     key = "main"
-    region = "us-east-1"
+    region = "eu-north-1"
     dynamodb_table = "brinda-dynamoDBtable"
   }
 }
