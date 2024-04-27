be90771d78b6a5cb53f29319f27a56b51b894896
IamBrinda <167959820+IamBrinda@users.noreply.github.com>
2024-04-27 21:41:08 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hashtek-terraform-state-bucket-230424"
+    bucket = "brinda-statefile"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "hashtekTerraformStateLock"
